36357b886a65ceff28aefee8bad2582732fd2ca6
AnesuMuusha <anesumuusha@gmail.com>
2023-02-27 20:34:24 +0200

    re-styled  footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer=()=>{
 return(
-    <div className=" footer d__flex align__items__center justify__content__space__between pz-10" style={{"padding":"10px 20px","zIndex":"100"}}>
+    <div className=" footer d__flex align__items__center justify__content__space__between pz-10" style={{" padding":"10px 20px","zIndex":"100","background-color": "black"}}>
 
-        <span className="copyright" style={{"color":"#c6c9d8","fontSize":"14px","opacity":"0.75"}}>Copyright @ MuushaKingDeveloper. All Rights Reserved.</span>
+        <span className="copyright" style={{"color":"white","padding-left":"16px","padding-bottom":"4px","fontSize":"14px","opacity":"0.75"}}>Copyright @Developer. All Rights Reserved.</span>
     </div>
 )
 };
