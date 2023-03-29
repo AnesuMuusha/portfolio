dcf188298c44905bb43090d0cf118122ef2e4250
AnesuMuusha <anesumuusha@gmail.com>
2023-03-29 12:59:37 +0200

    updated NavBar to appear on ever page

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Service from './components/Service';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/> 
     <Home/>
     <About/>
     <Service/>
